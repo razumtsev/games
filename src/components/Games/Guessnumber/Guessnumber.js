e933de59f1058221a26c2f2cd9545a177a44f7bc
Ilya Razumtsev <razumtsev.il@yandex.ru>
2024-05-03 16:21:58 +0300

    guessnumber tryes to work

--- src/components/Games/Guessnumber/Guessnumber.js
+++ src/components/Games/Guessnumber/Guessnumber.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './guessnumber.css';
+import PlayerNumber from './components/PlayerNumber/PlayerNumber';
 
 export default function Guessnumber() {
+  const [secretNumber, setSecretNumber] = useState('');
+  const [playerNumber, setPlayerNumber] = useState('');
+  const [attempts, setAttempts] = useState(7);
+
+  useEffect(() => {
+    setSecretNumber(randomNumber(1, 100));
+  }, []);
+
+  const randomNumber = (min, max) => {
+    const number = Math.floor(Math.random() * max) + min;
+    return number;
+  }
+
+  const makeGameInfo = () => {
+    if (attempts === 0) {
+      return `Попытки закончились. Вы проиграли. Было загадано число ${secretNumber}`
+    }
+    if (playerNumber === '') return `Количество попыток: ${attempts}`
+    else if (playerNumber === secretNumber) return `Вы угадали, было загадано число ${secretNumber}`
+    else if (playerNumber > secretNumber) return `Загаданное число меньше ${playerNumber}, осталось попыток: ${attempts}`
+    else return `Загаданное число больше ${playerNumber}, осталось попыток: ${attempts}`;
+  }
+
+  const gameInfo = makeGameInfo();
+
+
+
+  // const gameInfo = (playerNumber === '') 
+  //   ? `Количество попыток: ${attempts}` 
+  //   : (playerNumber === secretNumber) 
+  //     ? `Вы угадали, было загадано число ${secretNumber}` 
+  //     : (playerNumber > secretNumber) 
+  //       ? `Загаданное число меньше ${playerNumber}, осталось попыток: ${attempts}` 
+  //       : `Загаданное число больше ${playerNumber}, осталось попыток: ${attempts}`;
+
+  const handleSubmit = (number) => {
+    // if (attempts === 0) return;
+    setPlayerNumber(number);
+    setAttempts(attempts - 1);
+  }
+
   return (
-    <div>Guessnumber</div>
+    <div className='guessnumber'>
+      <h2 className='guessnumber__title'>Guessnumber</h2>
+      <p className='guessnumber__info'>Угадай число от 1 до 100.</p>
+      <p className='guessnumber__info'>{gameInfo}</p>
+      <PlayerNumber handleSubmit={handleSubmit}/>
+    </div>
   )
 }
---
+++ src/components/Games/Guessnumber/components/PlayerNumber/PlayerNumber.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './playernumber.css';
+
+export default function PlayerNumber({handleSubmit}) {
+  const [number, setNumber] = useState('');
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if(value > 100) {
+      setNumber(100);
+      return;
+    }
+    setNumber(value);
+  }
+
+  const handleButtonClick = () => {
+    handleSubmit(Number(number));
+    setNumber('');
+  }
+
+  return (
+    <div>
+      <input
+        className='playernumber'
+        type='number'
+        min='1'
+        max='100'
+        onChange={handleChange}
+        value={number}
+      />
+      <button
+        className='guessNumber__submit'
+        type='submit'
+        onClick={handleButtonClick}
+      >Проверить</button>
+    </div>
+  )
+}
