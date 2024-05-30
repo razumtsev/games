7429cc408e5c38e9dbc1de8436e00952a6806fe7
Ilya Razumtsev <razumtsev.il@yandex.ru>
2024-05-30 15:33:07 +0300

    main.js fixed

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD:src/components/Games/Games.js
-import './games.css';
-=======
 import './main.css';
->>>>>>> 2bf2d3aa34c77745597307cf02e760369dea8165:src/components/Main/Main.js
 import Notfound from './Notfound/Notfound';
 import Tictactoe from './Tictactoe/Tictactoe';
 import Rps from './Rps/Rps';
@@ -13,11 +9,7 @@ import Guessnumber from './Guessnumber/Guessnumber';
 
 export default function Games() {
   return (
-<<<<<<< HEAD:src/components/Games/Games.js
-    <div className='games'>
-=======
     <div className='main'>
->>>>>>> 2bf2d3aa34c77745597307cf02e760369dea8165:src/components/Main/Main.js
       <Routes>
         <Route path='/' element={<Contents />} />
         <Route path='/tictactoe' element={<Tictactoe />} />
